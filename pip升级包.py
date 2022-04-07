448d277d6ddcf4b30ef63bd54df519d428dc43a6
yxn <76934061+yxn4065@users.noreply.github.com>
2022-04-07 17:20:19 +0800

    Create pip升级包.py

---
+++ pip升级包.py
@@ -0,0 +1,9 @@
+import pip
+from subprocess import call
+from pip._internal.utils.misc import get_installed_distributions
+
+for dist in get_installed_distributions():
+    call("pip install --upgrade " + dist.project_name, shell=True)
+
+# 升级pip
+# python -m pip install --upgrade pip
